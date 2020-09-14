df09de44667ad9300694200a1fb5ea4a30b460f0
Taras Petruk <taras.petruk@gmail.com>
2020-09-14 17:03:18 +0300

    stub the MainView

---
+++ Toggl.WPF/Presentation/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Toggl.Core.UI.ViewModels;
+
+namespace Toggl.WPF.Presentation
+{
+    public partial class MainView
+    {
+        public MainView(MainViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+        }
+    }
+}
+
--- Toggl.WPF/Views/MainTabBarView.xaml.cs
+++ Toggl.WPF/Views/MainTabBarView.xaml.cs
@@ -1,21 +1,60 @@
-﻿using System.Reactive.Disposables;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Toggl.Core.UI.ViewModels;
+using Toggl.Shared.Extensions;
+using Toggl.WPF.Presentation;
 
 namespace Toggl.WPF.Views
 {
     public partial class MainTabBarView
     {
         private readonly CompositeDisposable disposeBag = new CompositeDisposable();
+
+        private readonly Dictionary<Type, Func<ViewModel, UserControl>> tabViews =
+            new Dictionary<Type, Func<ViewModel, UserControl>>
+            {
+                {typeof(MainViewModel), vm => new MainView((MainViewModel)vm)}
+            };
+
+        private readonly Dictionary<Type, string> tabHeaders =
+            new Dictionary<Type, string>
+            {
+                {typeof(MainViewModel), Toggl.Shared.Resources.Timer }
+            };
+
         public MainTabBarView(MainTabBarViewModel mainTabBarViewModel)
         {
             ViewModel = mainTabBarViewModel;
             InitializeComponent();
+
+            createTabFor(ViewModel.MainViewModel)
+                .GetAwaiter()
+                .GetResult();
+
+            async Task<UserControl> createTabFor(Lazy<ViewModel> lazyViewModel)
+            {
+                var viewModel = lazyViewModel.Value;
+                var viewModelType = viewModel.GetType();
+                if (!tabViews.ContainsKey(viewModelType)) return null;
+                await viewModel.Initialize();
+                var tabView = tabViews[viewModelType](viewModel);
+                var tabItem = new TabItem
+                {
+                    Header = tabHeaders[viewModelType],
+                    Content = tabView
+                };
+                TabControl.Items.Add(tabItem);
+                return tabView;
+            }
         }
 
         private void MainTabBarView_OnLoaded(object sender, RoutedEventArgs e)
         {
-
         }
     }
 }
