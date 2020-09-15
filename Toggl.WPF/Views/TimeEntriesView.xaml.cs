589605a15d2ff0e231f9cc8253c05c38d532d59d
Taras Petruk <taras.petruk@gmail.com>
2020-09-15 17:51:23 +0300

    WIP empty timeentriesview

---
+++ Toggl.WPF/Views/TimeEntriesView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Reactive.Disposables;
+using System.Windows;
+using System.Windows.Controls;
+using Toggl.Core.UI.Collections;
+using Toggl.Core.UI.ViewModels;
+using Toggl.Core.UI.ViewModels.MainLog;
+using Toggl.Core.UI.ViewModels.MainLog.Identity;
+
+namespace Toggl.WPF.Views
+{
+    public partial class TimeEntriesView
+    {
+        private readonly CompositeDisposable disposeBag = new CompositeDisposable();
+
+        public TimeEntriesView(TimeEntriesViewModel timeEntriesViewModel)
+        {
+            ViewModel = timeEntriesViewModel;
+            InitializeComponent();
+
+        }
+
+        private void TimeEntriesView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel.TimeEntries.Subscribe(UpdateTimeEntriesList)
+                .DisposeWith(disposeBag);
+        }
+
+        private void UpdateTimeEntriesList(
+            IImmutableList<AnimatableSectionModel<MainLogSectionViewModel, MainLogItemViewModel, IMainLogKey>> list)
+        {
+
+        }
+    }
+}
+
